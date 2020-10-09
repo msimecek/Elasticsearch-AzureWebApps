6cd676658622b3361e081cd75d66ef8b01f84c23
Martin Simecek <msimecek@users.noreply.github.com>
2020-10-09 18:09:47 +0200

    ARM template and modified set password script

---
+++ set_elastic_password.ps1
@@ -0,0 +1,20 @@
+﻿# Url of the Elasticsearch instnace on Azure Web Apps.
+$targetroot = "https://[your app].azurewebsites.net"
+# Username to change password for. Initially run for "elastic".
+$username = "elastic"
+# Password which was set during deployment.
+$bootstrapPass = "s3cr3t"
+# New password.
+$newPass = "[elastic new password]"
+
+# ---------------------
+
+$secpasswd = ConvertTo-SecureString $bootstrapPass -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($username, $secpasswd)
+
+$result = Invoke-RestMethod "$targetroot/_security/user/_password" -Method POST -Credential $credential -Body "{`"password`" : `"$newPass`"}" -ContentType "application/json"
+
+# ---------------------
+
+# Finally, use Azure CLI, portal or CI/CD to remove the ELASTIC_PASSWORD Application Setting from Azure.
+# az webapp config appsettings delete --name MyWebApp --resource-group MyResourceGroup --setting-names ELASTIC_PASSWORD
--- set_password.ps1
+++
@@ -1,9 +0,0 @@
-﻿$targetroot = "https://[your instance].azurewebsites.net"
-$username = "elastic"
-$bootstrapPass = "[your pass]"
-$newPass = "[new password]"
-
-$secpasswd = ConvertTo-SecureString $bootstrapPass -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($username, $secpasswd)
-
-$result = Invoke-RestMethod "$targetroot/_security/user/_password" -Method POST -Credential $credential -Body "{`"password`" : `"$newPass`"}" -ContentType "application/json"
